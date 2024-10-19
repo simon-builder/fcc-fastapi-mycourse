2cb4d316a086511ab703569ab2a80247f8869237
simon-builder <simonma.devs@gmail.com>
2024-10-19 20:12:19 +0200

    redeploy

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from .database import engine
 from .routers import post, user, auth, vote
 from .config import Settings
 
-### Replaced with Alembic
+#### Replaced with Alembic
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
