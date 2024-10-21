ed89f926d82e608f80c933e9a6e75b6b179504bf
simon-builder <simonma.devs@gmail.com>
2024-10-21 21:16:02 +0200

    test user login

---
+++ tests/conftest.py
@@ -0,0 +1,50 @@
+from fastapi.testclient import TestClient
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from app.main import app
+from app.config import settings
+from app.database import get_db, Base
+
+SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@pytest.fixture()
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@pytest.fixture()
+def client(session):
+    # Run code before passing Testclient
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+
+    # Run code after passing Testclient
+    #Base.metadata.drop_all(bind=engine)
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "hello123@gmail.com",
+                 "password": "password123"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data["password"]
+    return new_user
